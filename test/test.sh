92f773de7e37f911a42d22976ee19c6943e3dab2
takuyawww <wakataku.11010809@gmail.com>
2021-12-18 19:24:14 +0900

    整数一つをコンパイルするコンパイラを作成

---
+++ test/test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+assert() {
+  expected="$1"
+  input="$2"
+
+  ./9cc/9cc "$input" > ./9cc/tmp.s
+  cc -o ./9cc/tmp ./9cc/tmp.s
+  ./9cc/tmp
+  actual="$?"
+
+  if [ "$actual" = "$expected" ]; then
+    echo "$input => $actual"
+  else
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+assert 0 0
+assert 42 42
+
+echo OK

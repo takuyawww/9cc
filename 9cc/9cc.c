240c7229600d24805b158fa117dbed15fa0b334d
takuyawww <wakataku.11010809@gmail.com>
2021-12-23 22:42:56 +0900

    add unary

--- 9cc/9cc.c
+++ 9cc/9cc.c
@@ -4,11 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 //
 // Tokenizer
 //
-
 typedef enum {
   TK_RESERVED, // Keywords or punctuators
   TK_NUM,      // Integer literals
@@ -90,7 +88,6 @@ Token *tokenize() {
       p++;
       continue;
     }
-
     // Punctuator
     if (strchr("+-*/()", *p)) {
       cur = new_token(TK_RESERVED, cur, p++);
@@ -102,18 +99,14 @@ Token *tokenize() {
       cur->val = strtol(p, &p, 10);
       continue;
     }
-
     error_at(p, "invalid token");
   }
-
   new_token(TK_EOF, cur, p);
   return head.next;
 }
-
 //
 // Parser
 //
-
 typedef enum {
   ND_ADD, // +
   ND_SUB, // -
@@ -121,7 +114,6 @@ typedef enum {
   ND_DIV, // /
   ND_NUM, // Integer
 } NodeKind;
-
 // AST node type
 typedef struct Node Node;
 struct Node {
@@ -130,20 +122,17 @@ struct Node {
   Node *rhs;     // Right-hand side
   int val;       // Used if kind == ND_NUM
 };
-
 Node *new_node(NodeKind kind) {
   Node *node = calloc(1, sizeof(Node));
   node->kind = kind;
   return node;
 }
-
 Node *new_binary(NodeKind kind, Node *lhs, Node *rhs) {
   Node *node = new_node(kind);
   node->lhs = lhs;
   node->rhs = rhs;
   return node;
 }
-
 Node *new_num(int val) {
   Node *node = new_node(ND_NUM);
   node->val = val;
@@ -152,12 +141,12 @@ Node *new_num(int val) {
 
 Node *expr();
 Node *mul();
+Node *unary();
 Node *primary();
 
 // expr = mul ("+" mul | "-" mul)*
 Node *expr() {
   Node *node = mul();
-
   for (;;) {
     if (consume('+'))
       node = new_binary(ND_ADD, node, mul());
@@ -168,18 +157,28 @@ Node *expr() {
   }
 }
 
-// mul = primary ("*" primary | "/" primary)*
+// mul = unary ("*" unary | "/" unary)*
 Node *mul() {
-  Node *node = primary();
+  Node *node = unary();
 
   for (;;) {
     if (consume('*'))
-      node = new_binary(ND_MUL, node, primary());
+      node = new_binary(ND_MUL, node, unary());
     else if (consume('/'))
-      node = new_binary(ND_DIV, node, primary());
+      node = new_binary(ND_DIV, node, unary());
     else
       return node;
   }
+}
+
+// unary = ("+" | "-")? unary
+//       | primary
+Node *unary() {
+  if (consume('+'))
+    return unary();
+  if (consume('-'))
+    return new_binary(ND_SUB, new_num(0), unary());
+  return primary();
 }
 
 // primary = "(" expr ")" | num
@@ -189,26 +188,20 @@ Node *primary() {
     expect(')');
     return node;
   }
-
   return new_num(expect_number());
 }
-
 //
 // Code generator
 //
-
 void gen(Node *node) {
   if (node->kind == ND_NUM) {
     printf("  push %d\n", node->val);
     return;
   }
-
   gen(node->lhs);
   gen(node->rhs);
-
   printf("  pop rdi\n");
   printf("  pop rax\n");
-
   switch (node->kind) {
   case ND_ADD:
     printf("  add rax, rdi\n");
@@ -224,27 +217,21 @@ void gen(Node *node) {
     printf("  idiv rdi\n");
     break;
   }
-
   printf("  push rax\n");
 }
-
 int main(int argc, char **argv) {
   if (argc != 2)
     error("%s: invalid number of arguments", argv[0]);
-
   // Tokenize and parse.
   user_input = argv[1];
   token = tokenize();
   Node *node = expr();
-
   // Print out the first half of assembly.
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
-
   // Traverse the AST to emit assembly.
   gen(node);
-
   // A result must be at the top of the stack, so pop it
   // to RAX to make it a program exit code.
   printf("  pop rax\n");

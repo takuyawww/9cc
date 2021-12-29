413d385f740172c588eb8172c80d0ffce7d28668
takuyawww <wakataku.11010809@gmail.com>
2021-12-29 13:02:26 +0900

    add lval statement

--- 9cc.h
+++ 9cc.h
@@ -1,3 +1,4 @@
+#define _POSIX_C_SOURCE 200809L
 #include <assert.h>
 #include <ctype.h>
 #include <stdarg.h>
@@ -5,10 +6,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+typedef struct Node Node;
+
 //
 // tokenize.c
 //
 
+// Token
 typedef enum {
   TK_IDENT, // Identifiers
   TK_PUNCT, // Punctuators
@@ -33,6 +38,24 @@ Token *tokenize(char *input);
 //
 // parse.c
 //
+
+// Local variable
+typedef struct Obj Obj;
+struct Obj {
+  Obj *next;
+  char *name; // Variable name
+  int offset; // Offset from RBP
+};
+
+// Function
+typedef struct Function Function;
+struct Function {
+  Node *body;
+  Obj *locals;
+  int stack_size;
+};
+
+// AST node
 typedef enum {
   ND_ADD,       // +
   ND_SUB,       // -
@@ -50,18 +73,19 @@ typedef enum {
 } NodeKind;
 
 // AST node type
-typedef struct Node Node;
 struct Node {
   NodeKind kind; // Node kind
   Node *next;    // Next node
   Node *lhs;     // Left-hand side
   Node *rhs;     // Right-hand side
-  char name;     // Used if kind == ND_VAR
+  Obj *var;      // Used if kind == ND_VAR
   int val;       // Used if kind == ND_NUM
 };
 
-Node *parse(Token *tok);
+Function *parse(Token *tok);
+
 //
 // codegen.c
 //
-void codegen(Node *node);
+
+void codegen(Function *prog);
--- parse.c
+++ parse.c
@@ -1,5 +1,9 @@
 #include "9cc.h"
 
+// All local variable instances created during parsing are
+// accumulated to this list.
+Obj *locals;
+
 static Node *expr(Token **rest, Token *tok);
 static Node *expr_stmt(Token **rest, Token *tok);
 static Node *assign(Token **rest, Token *tok);
@@ -9,6 +13,15 @@ static Node *add(Token **rest, Token *tok);
 static Node *mul(Token **rest, Token *tok);
 static Node *unary(Token **rest, Token *tok);
 static Node *primary(Token **rest, Token *tok);
+
+// Find a local variable by name.
+static Obj *find_var(Token *tok) {
+  for (Obj *var = locals; var; var = var->next)
+    if (strlen(var->name) == tok->len && !strncmp(tok->loc, var->name, tok->len))
+      return var;
+  return NULL;
+}
+
 static Node *new_node(NodeKind kind) {
   Node *node = calloc(1, sizeof(Node));
   node->kind = kind;
@@ -31,10 +44,18 @@ static Node *new_num(int val) {
   return node;
 }
 
-static Node *new_var_node(char name) {
+static Node *new_var_node(Obj *var) {
   Node *node = new_node(ND_VAR);
-  node->name = name;
+  node->var = var;
   return node;
+}
+
+static Obj *new_lvar(char *name) {
+  Obj *var = calloc(1, sizeof(Obj));
+  var->name = name;
+  var->next = locals;
+  locals = var;
+  return var;
 }
 
 // stmt = expr-stmt
@@ -47,12 +68,10 @@ static Node *expr_stmt(Token **rest, Token *tok) {
   *rest = skip(tok, ";");
   return node;
 }
-
 // expr = assign
 static Node *expr(Token **rest, Token *tok) {
   return assign(rest, tok);
 }
-
 // assign = equality ("=" assign)?
 static Node *assign(Token **rest, Token *tok) {
   Node *node = equality(&tok, tok);
@@ -61,7 +80,6 @@ static Node *assign(Token **rest, Token *tok) {
   *rest = tok;
   return node;
 }
-
 // equality = relational ("==" relational | "!=" relational)*
 static Node *equality(Token **rest, Token *tok) {
   Node *node = relational(&tok, tok);
@@ -143,7 +161,6 @@ static Node *unary(Token **rest, Token *tok) {
     return new_unary(ND_NEG, unary(rest, tok->next));
   return primary(rest, tok);
 }
-
 // primary = "(" expr ")" | ident | num
 static Node *primary(Token **rest, Token *tok) {
   if (equal(tok, "(")) {
@@ -153,9 +170,11 @@ static Node *primary(Token **rest, Token *tok) {
   }
 
   if (tok->kind == TK_IDENT) {
-    Node *node = new_var_node(*tok->loc);
+    Obj *var = find_var(tok);
+    if (!var)
+      var = new_lvar(strndup(tok->loc, tok->len));
     *rest = tok->next;
-    return node;
+    return new_var_node(var);
   }
 
   if (tok->kind == TK_NUM) {
@@ -165,11 +184,17 @@ static Node *primary(Token **rest, Token *tok) {
   }
   error_tok(tok, "expected an expression");
 }
+
 // program = stmt*
-Node *parse(Token *tok) {
+Function *parse(Token *tok) {
   Node head = {};
   Node *cur = &head;
+
   while (tok->kind != TK_EOF)
     cur = cur->next = stmt(&tok, tok);
-  return head.next;
+
+  Function *prog = calloc(1, sizeof(Function));
+  prog->body = head.next;
+  prog->locals = locals;
+  return prog;
 }
